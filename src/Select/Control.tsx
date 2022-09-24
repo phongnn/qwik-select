aaa642fe14d4e5c02422ec995aaf3b0a22ce79f6
Phong Nguyen <phongnn@gmail.com>
2022-09-24 11:56:28 +0700

    fix: don't show menu after clearing selected value

--- src/Select/Control.tsx
+++ src/Select/Control.tsx
@@ -1,4 +1,11 @@
-import { MutableWrapper, PropFunction, Ref } from "@builder.io/qwik";
+import {
+  MutableWrapper,
+  PropFunction,
+  Ref,
+  component$,
+  useClientEffect$,
+  useRef,
+} from "@builder.io/qwik";
 
 interface ControlProps {
   ref: Ref<HTMLInputElement>;
@@ -30,29 +37,39 @@ const Control = (props: ControlProps) => {
   );
 };
 
-const ClearButton = (props: { onClick$: PropFunction<() => void> }) => {
-  return (
-    <div
-      class="clear-button"
-      onClick$={props.onClick$}
-      data-testid="qwik-select-clear"
-    >
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="-2 -2 50 50"
-        focusable="false"
-        aria-hidden="true"
-        role="presentation"
-      >
-        <path
-          fill="currentColor"
-          d="M34.923,37.251L24,26.328L13.077,37.251L9.436,33.61l10.923-10.923L9.436,11.765l3.641-3.641L24,19.047L34.923,8.124
+const ClearButton = component$(
+  (props: { onClick$: PropFunction<() => void> }) => {
+    // we use synchronous event here to stop it from propagating
+    // to the container which would open the menu
+    const ref = useRef<HTMLElement>();
+    useClientEffect$(() => {
+      const handler = (event: Event) => {
+        event.stopPropagation();
+        props.onClick$();
+      };
+      ref.current!.addEventListener("click", handler);
+      return () => ref.current!.removeEventListener("click", handler);
+    });
+
+    return (
+      <div class="clear-button" ref={ref} data-testid="qwik-select-clear">
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="-2 -2 50 50"
+          focusable="false"
+          aria-hidden="true"
+          role="presentation"
+        >
+          <path
+            fill="currentColor"
+            d="M34.923,37.251L24,26.328L13.077,37.251L9.436,33.61l10.923-10.923L9.436,11.765l3.641-3.641L24,19.047L34.923,8.124
 l3.641,3.641L27.641,22.688L38.564,33.61L34.923,37.251z"
-        />
-      </svg>
-    </div>
-  );
-};
+          />
+        </svg>
+      </div>
+    );
+  }
+);
 
 export default Control;
