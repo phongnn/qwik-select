8965433dfcb860c3c7b4c3fd0bf4063280ec0a95
Phong Nguyen <phongnn@gmail.com>
2022-10-13 11:25:31 +0700

    style: remove tailwind

--- app/postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  plugins: {
-    tailwindcss: { config: path.resolve(__dirname, "tailwind.config.cjs") },
-    autoprefixer: {},
-  },
-};
--- app/tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./app/src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
